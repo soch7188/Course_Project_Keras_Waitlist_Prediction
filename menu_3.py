2f625f344a19fc6fd3dbfea24c0a6e61b4b3ca94
Hu Yao-Chieh <yhuag@connect.ust.hk>
2018-02-20 17:56:45 +0800

    menu 3 template

--- menu_3.py
+++ menu_3.py
@@ -1,15 +1,40 @@
+import os
+
 def run():
     # Welcome message
-    print("Hello Menu 3 !\n")
+    print("(3) Course Search\n")
 
-    # Pre-execution menu
-    # print("9. Back")
-    # print("0. Quit")
-    # choice = input(" >>  ")
-    # exec_menu(choice)
+    print("Please choose the menu you want to start by entering the number")
+    print("[1] Course Search by Keyword")
+    print("[2] Course Search by Waiting List Size")
+    choice = input(" >>  ")
+    exec_menu(choice)
+
 
     # Main execution
-    print("Collection dropping and empty collection creating are successful")
+    # print("Data Crawling is successful and all data are inserted into the database")
     print("Warning: This is a stub.\n")
+
+    return
+
+def searchByKeyword():
+    print("Course Search by Keyword...")
+    return
+
+def searchByWaitingListSize():
+    print("Course Search by Keyword...")
+    return
+
+# Execute menu
+def exec_menu(choice):
+    os.system('clear')
+    ch = choice.lower()
+
+    if ch == '1':
+        searchByKeyword()
+    elif ch == '2':
+        searchByWaitingListSize()
+    else:
+        print("Please retry.")
 
     return
