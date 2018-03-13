9102835274fa57adba72b5afb909e6c2232092d5
soch7188 <soch7188@naver.com>
2018-03-13 16:03:18 +0800

    refactor

--- query_waitlist.js
+++ query_waitlist.js
@@ -1,26 +1,14 @@
 db = db.getSiblingDB("university")
 
-// INDEX BASED SEARCH
-// indexResult = db.course.find({$text: {$search: "Multimedia asdfasdf"}},
-//     // Project
-//     {
-//         _id: 0,
-//         cid: 1,
-//         cname: 1,
-//         credit: 1,
-//         sectionList: 1
-//     }
-// ).sort({'timeRecord':1})
-
-// printjson(indexResult.toArray());
+start_ts = ISODate("2018-01-26T00:00:00.000Z")
+end_ts = ISODate("2018-01-28T00:00:00.000Z")
 
 result = db.course.aggregate(
     [
-        
         { $unwind: '$sectionList'},
         { $match: {"sectionList.timeSlot": {
-            $gte: ISODate("2018-01-26T00:00:00.000Z"),
-            $lt: ISODate("2018-01-27T00:00:00.000Z")
+            $gte: start_ts,
+            $lt: end_ts
         }}},
         // { $sort: {"sectionList.section": 1 } },
         { $project : { _id: 0, cid: 1, cname: 1, "sectionList.section": 1, "sectionList.wait": 1, "sectionList.timeSlot": 1 } },
