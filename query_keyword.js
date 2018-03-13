dbda5c703f860cce4e5b1ec46a9094ce9ff84f20
soch7188 <soch7188@naver.com>
2018-03-13 14:02:47 +0800

    Search by Keyword Complete.

--- query_keyword.js
+++ query_keyword.js
@@ -1,48 +1,12 @@
 db = db.getSiblingDB("university")
 
-db.course.createIndex({"$**":"text"})
+// db.course.createIndex({"$**": "text"})
+db.course.createIndex({"cname": "text"})
 
-indexResult = db.course.find({$text: {$search: "\"course\""}}, {}).sort({'timeRecord':1})
+// INDEX BASED SEARCH
+indexResult = db.course.find({$text: {$search: "\'Multimedi\'"}}, {}).sort({'timeRecord':1})
 
-// result = db.course.aggregate(
-//     [
-//         {
-//             $lookup: {
-//                 localField: 'section_id',
-//                 from: 'section',
-//                 foreignField: 'section_id',
-//                 as: 'lecture_section'
-//             }
-//         },
-//         {
-//             $project: {
-//                 _id: 1,
-//                 'Course Code': '$course_code',
-//                 'Course Title': '$title',
-//                 'No.ofUnits/Credits': '$credits',
-//                 'lecture_section.code': 1,
-//                 'lecture_section.datetime': 1,
-//                 'lecture_section.quota': 1,
-//                 'lecture_section.enrol': 1,
-//                 'lecture_section.avail': 1,
-//                 'lecture_section.wait': 1
-//             }
-//         },
-//         {
-//             $sort: {'course_code': 1, 'lecture_section.section_id': 1}
-//         }
-//     ]
-// )
+// REGEX BASED SEARCH
+// indexResult = db.course.find({"cname": {$regex: /Multimedi/}})
 
 printjson(indexResult.toArray());
-// printjson(result.toArray());
-
-
-// printjson( db.course.find( {description: { $regex: "course"} } ).toArray() );
-
-// db.course.find( {description: { $regex: "course"} } );
-//
-// cursor = db.course.find( {description: { $regex: "course"} } );
-// while ( cursor.hasNext() ) {
-//    print( cursor.next() )
-// }
