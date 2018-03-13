2c5f28cbe1c6f22de20639e18bf8018c0f7daa88
soch7188 <soch7188@naver.com>
2018-03-13 13:46:05 +0800

    query_keyword updated

--- query_keyword.js
+++ query_keyword.js
@@ -1,39 +1,44 @@
 db = db.getSiblingDB("university")
 
-result = db.course.aggregate(
-    [
-        {
-            $lookup: {
-                localField: 'section_id',
-                from: 'section',
-                foreignField: 'section_id',
-                as: 'lecture_section'
-            }
-        },
-        {
-            $project: {
-                _id: 1,
-                'Course Code': '$course_code',
-                'Course Title': '$title',
-                'No.ofUnits/Credits': '$credits',
-                'lecture_section.code': 1,
-                'lecture_section.datetime': 1,
-                'lecture_section.quota': 1,
-                'lecture_section.enrol': 1,
-                'lecture_section.avail': 1,
-                'lecture_section.wait': 1
-            }
-        },
-        {
-            $sort: {'course_code': 1, 'lecture_section.section_id': 1}
-        }
-    ]
-)
+db.course.createIndex({"$**":"text"})
 
-printjson(result.toArray());
+indexResult = db.course.find({$text: {$search: "\"course\""}}, {}).sort({'timeRecord':1})
+
+// result = db.course.aggregate(
+//     [
+//         {
+//             $lookup: {
+//                 localField: 'section_id',
+//                 from: 'section',
+//                 foreignField: 'section_id',
+//                 as: 'lecture_section'
+//             }
+//         },
+//         {
+//             $project: {
+//                 _id: 1,
+//                 'Course Code': '$course_code',
+//                 'Course Title': '$title',
+//                 'No.ofUnits/Credits': '$credits',
+//                 'lecture_section.code': 1,
+//                 'lecture_section.datetime': 1,
+//                 'lecture_section.quota': 1,
+//                 'lecture_section.enrol': 1,
+//                 'lecture_section.avail': 1,
+//                 'lecture_section.wait': 1
+//             }
+//         },
+//         {
+//             $sort: {'course_code': 1, 'lecture_section.section_id': 1}
+//         }
+//     ]
+// )
+
+printjson(indexResult.toArray());
+// printjson(result.toArray());
 
 
-printjson( db.course.find( {description: { $regex: "course"} } ).toArray() );
+// printjson( db.course.find( {description: { $regex: "course"} } ).toArray() );
 
 // db.course.find( {description: { $regex: "course"} } );
 //
