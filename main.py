4f4131a2e18538ebea6e05170550f9af5d884c59
Hu Yao-Chieh <yhuag@connect.ust.hk>
2018-03-13 13:22:28 +0800

    insert via python is ok

--- main.py
+++ main.py
@@ -23,18 +23,71 @@ import menu_1,menu_2,menu_3,menu_4,menu_5
 # Import Python-MongoDB component
 # Remember to start the MongoDB server before running this Python script
 from pymongo import MongoClient
+# from datetime import *
 
 
 # Try to insert a document
 def insertDocument(db):
     try:
-        db.student.insert(
+        db.course.insert({
+            "cid": "COMP 1001",
+            "cname": "Exploring Multimedia and Internet Computing",
+            "category": "COMP",
+            "credit": 3,
+            "courseInfo": 
             {
-                "sid": "11112222",
-                "sname": "Ziwon",
-                "byear": 1998
-            }
-        )
+                "attributes": 
+                [
+                    "Common Core (S&T) for 2010 & 2011 3Y programs",
+                    "Common Core (S&T) for 2012 3Y programs",
+                    "Common Core (S&T) for 4Y programs"
+                ],
+                "exclusion": "ISOM 2010, any COMP courses of 2000-level or above",
+                "description": "This course is an introduction to computers and computing tools. It introduces the organization and basic working mechanism of a computer system, including the development of the trend of modern computer system. It covers the fundamentals of computer hardware design and software application development. The course emphasizes the application of the state-of-the-art software tools to solve problems and present solutions via a range of skills related to multimedia and internet computing tools such as internet, e-mail, WWW, webpage design, computer animation, spread sheet charts/figures, presentations with graphics and animations, etc. The course also covers business, accessibility, and relevant security issues in the use of computers and Internet."
+            },      
+            "sectionList":
+            [
+                {
+                    "section": "L1",
+                    "sectionNumber": 1756,
+                    "dateTime": 
+                    {
+
+                        "weekDay": "Thursday",
+                        "startTime": "03:00PM",
+                        "endTime": "04:50PM",
+                    },
+                    "room": "Rm 5620, Lift 31-32 (70)",
+                    "instructor": "LEUNG, Wai Ting",
+                    "quota": 67,
+                    "enrol": 4,
+                    "avail": 63,
+                    "wait": 0,
+                    "remarks": "",
+                    "timeRecord": "25 January 2018, 09:00"
+                },
+                {
+                    "section": "LA1",
+                    "sectionNumber": 1757,
+                    "dateTime": 
+                    {
+
+                        "weekDay": "Tuesday",
+                        "startTime": "03:00PM",
+                        "endTime": "04:50PM",
+
+                    },
+                    "room": "Rm 4210, Lift 19 (67)",
+                    "instructor": "LEUNG, Wai Ting",
+                    "quota": 67,
+                    "enrol": 4,
+                    "avail": 63,
+                    "wait": 0,
+                    "remarks": "",
+                    "timeRecord": "25 January 2018, 09:00"
+                }
+            ]
+        })
     except pymongo.errors.ConnectionFailure as error: 
         print("Document Insertion Failed! Error Message: \"{}\"".format(error))
 
