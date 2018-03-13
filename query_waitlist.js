57441df70b567688c6556c507435886ba24bdd60
Hu Yao-Chieh <yhuag@connect.ust.hk>
2018-03-13 15:59:00 +0800

    complete date search

--- query_waitlist.js
+++ query_waitlist.js
@@ -1,25 +1,29 @@
 db = db.getSiblingDB("university")
 
 // INDEX BASED SEARCH
-indexResult = db.course.find({$text: {$search: "Multimedia asdfasdf"}},
-    // Project
-    {
-        _id: 0,
-        cid: 1,
-        cname: 1,
-        credit: 1,
-        sectionList: 1
-    }
-).sort({'timeRecord':1})
+// indexResult = db.course.find({$text: {$search: "Multimedia asdfasdf"}},
+//     // Project
+//     {
+//         _id: 0,
+//         cid: 1,
+//         cname: 1,
+//         credit: 1,
+//         sectionList: 1
+//     }
+// ).sort({'timeRecord':1})
 
-printjson(indexResult.toArray());
+// printjson(indexResult.toArray());
 
 result = db.course.aggregate(
     [
-        {$match: {$text: {$search: 'Exploring asdasdf'}}},
+        
         { $unwind: '$sectionList'},
-        { $sort: {"sectionList.section": 1 } },
-        { $project : { _id: 0, cid: 1, cname: 1, credit: 1, "sectionList.section": 1, "sectionList.dateTime": 1, "sectionList.quota": 1, "sectionList.enrol": 1, "sectionList.avail": 1, "sectionList.wait": 1 } },
+        { $match: {"sectionList.timeSlot": {
+            $gte: ISODate("2018-01-26T00:00:00.000Z"),
+            $lt: ISODate("2018-01-27T00:00:00.000Z")
+        }}},
+        // { $sort: {"sectionList.section": 1 } },
+        { $project : { _id: 0, cid: 1, cname: 1, "sectionList.section": 1, "sectionList.wait": 1, "sectionList.timeSlot": 1 } },
     ]
 )
 
