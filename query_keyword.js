3b11f9333f4888a8c0588ffebc7a637f9a54c69b
soch7188 <soch7188@naver.com>
2018-03-13 15:42:15 +0800

    cleanup

--- query_keyword.js
+++ query_keyword.js
@@ -7,17 +7,6 @@ db = db.getSiblingDB("university")
 db.course.createIndex({"cname": "text"})
 
 // INDEX BASED SEARCH
-// indexResult = db.course.find({$text: {$search: "Multimedia asdfasdf"}},
-//     // Project
-//     {
-//         _id: 0,
-//         cid: 1,
-//         cname: 1,
-//         credit: 1,
-//         sectionList: 1
-//     }
-// ).sort({'sectionList.section': 1})
-
 indexAggregateSortedResult = db.course.aggregate(
     [
         {$match: {$text: {$search: 'Exploring asdasdf'}}},
