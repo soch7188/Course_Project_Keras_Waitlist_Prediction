3d4ea15ad20fd9690f713011edecc8a3e822fcff
soch7188 <soch7188@naver.com>
2018-03-15 15:26:32 +0800

    Update & Cleanup

---
+++ query.js
@@ -0,0 +1,111 @@
+db = db.getSiblingDB("university")
+
+// ------------------ Query by Keyword --------------------- //
+print(" -------------- Query by Keyword --------------- ")
+
+
+// Creating a text index for all fields.
+// db.course.createIndex({"$**": "text"})
+
+// Creating a text index for "cname" field.
+db.course.createIndex({"cname": "text"})
+
+// INDEX BASED SEARCH
+indexAggregateSortedResult = db.course.aggregate(
+    [
+        { $match: {$text: {$search: 'Exploring asdasdf'}}},
+        { $unwind: '$sectionList'},
+        { $sort: {"cid": -1, "sectionList.section": 1}},
+        { $project : { _id: 0, cid: 1, cname: 1, credit: 1, "sectionList.section": 1, "sectionList.dateTime": 1, "sectionList.quota": 1, "sectionList.enrol": 1, "sectionList.avail": 1, "sectionList.wait": 1 } },
+        { $group: {
+            "_id": "$cid",
+            "Course Code": {"$last": "$cid"},
+            "Course Title": {"$last": "$cname"},
+            "Number of Units/Credits": {"$last": "$credit"},
+            "Section_List": {"$push" :"$sectionList"},
+        }},
+        {$project: {_id: 0}}
+    ]
+);
+
+// REGEX BASED SEARCH
+// indexResult = db.course.find({"cname": {$regex: /Multimedi/}})
+
+printjson(indexAggregateSortedResult.toArray());
+
+
+
+
+
+
+// -------------- Query by Waitlist Size --------------- //
+print("\n\n\n -------------- Query by Waitlist Size --------------- ")
+
+num_f = 2;
+start_ts = ISODate("2018-01-24T00:00:00.000Z");
+end_ts = ISODate("2018-01-29T00:00:00.000Z");
+
+latestTimeSlotResult = db.course.aggregate([
+    { $unwind: '$sectionList'},
+    { $match: {
+        "sectionList.timeSlot": {
+            $gte: start_ts,
+            $lt: end_ts
+        }
+    }},
+    // { $match: {$expr: {$gt: ["$sectionList.wait", {$multiply: ["$sectionList.enrol", num_f]}]}}},
+    { $group: {
+        "_id": "$cid",
+        "LatestTimeSlot": {
+            "$last": "$sectionList.timeSlot"
+        }
+    }}
+]);
+
+// printjson(latestTimeSlotResult.toArray())
+latestTimeSlot = latestTimeSlotResult.toArray()[0].LatestTimeSlot
+
+result = db.course.aggregate([
+        { $unwind: '$sectionList'},
+        { $match: {"sectionList.timeSlot": {
+            $gte: start_ts,
+            $lt: end_ts
+        }}},
+        // { $match: {$expr: {$gt: ["$sectionList.wait", {$multiply: ["$sectionList.enrol", num_f]}]}}},
+        { $project : {
+            _id: 0,
+            cid: 1,
+            cname: 1,
+            credit: 1,
+            // "sectionList.Satisfied": {
+            //     $cond: {
+            //         if: {$gt: ["$sectionList.wait", {$multiply: ["$sectionList.enrol", num_f]}]},
+            //         then: "Yes",
+            //         else: "No"
+            //     }
+            // },
+            "sectionList.section": 1,
+            "sectionList.dateTime": 1,
+            "sectionList.quota": 1,
+            "sectionList.enrol": 1,
+            "sectionList.avail": 1,
+            "sectionList.wait": 1,
+            "sectionList.timeSlot": 1,
+            }
+        },
+        { $sort: {"cid": -1, "sectionList.section": 1}},
+        { $match: { "sectionList.timeSlot": latestTimeSlot }},
+        { $group: {
+            "_id": "$cid",
+            "Course Code": {"$last": "$cid"},
+            "Course Title": {"$last": "$cname"},
+            "Number of Units/Credits": {"$last": "$credit"},
+            "Matched Time Slot": {"$last": latestTimeSlot},
+            "Section_List": {"$push" :"$sectionList"},
+        }},
+        {$project: {_id: 0}}
+    ]
+);
+
+
+printjson(result.toArray());
--- query_keyword.js
+++ query_keyword.js
@@ -9,10 +9,18 @@ db.course.createIndex({"cname": "text"})
 // INDEX BASED SEARCH
 indexAggregateSortedResult = db.course.aggregate(
     [
-        {$match: {$text: {$search: 'Exploring asdasdf'}}},
+        { $match: {$text: {$search: 'Exploring asdasdf'}}},
         { $unwind: '$sectionList'},
-        { $sort: {"sectionList.section": 1 } },
+        { $sort: {"cid": -1, "sectionList.section": 1}},
         { $project : { _id: 0, cid: 1, cname: 1, credit: 1, "sectionList.section": 1, "sectionList.dateTime": 1, "sectionList.quota": 1, "sectionList.enrol": 1, "sectionList.avail": 1, "sectionList.wait": 1 } },
+        { $group: {
+            "_id": "$cid",
+            "Course Code": {"$last": "$cid"},
+            "Course Title": {"$last": "$cname"},
+            "Number of Units/Credits": {"$last": "$credit"},
+            "Section_List": {"$push" :"$sectionList"},
+        }},
+        {$project: {_id: 0}}
     ]
 )
 
