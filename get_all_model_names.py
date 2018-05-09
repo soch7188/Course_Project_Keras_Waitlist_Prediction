fb408ffabc2187d1849631ca55928ec32cf557f4
Hu Yao-Chieh <yhuag@connect.ust.hk>
2018-05-10 02:31:32 +0800

    collect all the name of the course and section to be trained

--- get_all_model_names.py
+++ get_all_model_names.py
@@ -13,16 +13,21 @@ print("Database Connected")
 
 def arr(_in): return [x for x in _in]
 
-def mergeDict(x,y): return {**x, **y}
+# def mergeDict(x,y): return {**x, **y}
 
 
 course = arr(db.courses.aggregate([
     {"$match": {"code": {"$regex": "RMBI.*|COMP1942|COMP42.*|COMP43.*"}}},
     {"$project": {"code":1, "_id":0, "sections.sectionId":1}},
     {"$unwind": "$sections"},
-    {"$project": {"course":"$_id"}} 
+    {"$group": {
+        "_id":{
+            "code":"$code",
+            "sectionId":"$sections.sectionId"
+    }}} 
 ]))
 
-print(course)
+# print(course)
 
-# , "_id":0, "lecture":"$sections.sectionId"
+courseList = [x["_id"] for x in arr(course)]
+print(courseList)
